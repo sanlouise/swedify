fc850f621a0bc926333eb8cfc8c1c4b499b99608
lydiahallie <lydiajuliettehallie@gmail.com>
2017-07-29 17:04:37 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+const stockholm = document.getElementById("stockholm");
+const goteborg = document.getElementById("goteborg");
+const otherCity = document.getElementById("other");
+
+stockholm.addEventListener("click", () => {
+  window.location = "main.html";
+}
