827dbf6973e75c67c18c0df9030a1efd9c32c9a5
sanlouise <sandra.adamshallie@gmail.com>
2017-08-04 13:26:55 -0400

    Reorganize files, update file paths

--- server.js
+++ server.js
@@ -2,12 +2,17 @@ const express = require('express');
 const mustacheExpress = require('mustache-express');
 const bodyParser = require('body-parser');
 const app = express();
+const fs = require('fs');
 
 app.use(bodyParser.json());
 
 app.engine('mustache', mustacheExpress());
 app.set('views', './views');
 app.set('view engine', 'mustache')
+
+app.use(express.static('./public'));
+
+let sentences = JSON.parse(fs.readFileSync('./sentences.json', 'utf8'));
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
